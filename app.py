ea419e42722cba34e45dccf6b0cba3332d1a0ecc
chick_0 <chick_0@ch1ck.xyz>
2022-07-15 21:43:11 +0900

    update init code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from sys import argv
 from os.path import join
 
 from fastapi import FastAPI
+from fastapi import APIRouter
 
 try:
     version = open(join(".git", open(join(".git", "HEAD"), mode="r").read()[5:].strip()), mode="r").read()[:7]
@@ -14,3 +15,12 @@ app = FastAPI(
     version=version,
     openapi_url="/openapi.json" if '--dev' in argv else None
 )
+
+api_versions = [
+    "v1",
+]
+
+for v in [__import__(x) for x in api_versions]:
+    router: APIRouter = getattr(v, "init")()
+    if not router.deprecated:
+        app.include_router(router=router)
