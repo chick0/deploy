76f13dd3f996bbd0639050d839ae45408fdc4e8b
chick_0 <chick_0@ch1ck.xyz>
2022-07-17 15:50:47 +0900

    create sql models

---
+++ sql/models.py
@@ -0,0 +1,95 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import SmallInteger
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    uuid = Column(
+        String(36),
+        unique=True,
+        primary_key=False,
+        nullable=False
+    )
+
+    email = Column(
+        String(120),
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+
+    password = Column(
+        String(128),
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime,
+        nullable=False,
+    )
+
+    last_login_at = Column(
+        DateTime,
+        nullable=False,
+    )
+
+    def __repr__(self):
+        return f"<User uuid={self.uuid!r}, email={self.email!r}>"
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    uuid = Column(
+        String(36),
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+
+    title = Column(
+        String(120),
+        nullable=False
+    )
+
+    owner = Column(
+        String(36),
+        ForeignKey("user.uuid")
+    )
+
+    created_at = Column(
+        DateTime,
+        nullable=False,
+    )
+
+    deploy_at = Column(
+        DateTime,
+        nullable=True,
+    )
+
+    deploy_by = Column(
+        String(36),
+        ForeignKey("user.uuid")
+    )
+
+    # utils.type.ProjectType
+    type = Column(
+        SmallInteger,
+        nullable=False,
+    )
+
+    # ignored in front project
+    path = Column(
+        String(255),
+        nullable=True
+    )
+
+    def __repr__(self):
+        return f"<Project uuid={self.uuid!r} owner={self.owner!r}, title={self.title!r}>"
---
+++ utils/type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class ProjectType(Enum):
+    FRONTEND = 0
+    BACKEND = 1
+    BOT = 2
