1c109ee2de5ec42c9553fd4a49ee6cdb935f36bf
chick_0 <chick_0@ch1ck.xyz>
2022-07-17 19:11:58 +0900

    create iat and exp in auth token

--- utils/token/__init__.py
+++ utils/token/__init__.py
@@ -1,7 +1,11 @@
 from os import environ
+from datetime import datetime
+from datetime import timedelta
 
 key = environ['JWT_SECRET']
 algorithms = ['HS256']
 
 # registered claim #
 iss = environ['ISS']
+iat = lambda: datetime.now()
+exp = lambda: datetime.now() + timedelta(hours=3)
--- utils/token/auth.py
+++ utils/token/auth.py
@@ -7,18 +7,24 @@ from pydantic import BaseModel
 from utils.token import key
 from utils.token import algorithms
 from utils.token import iss
+from utils.token import iat
+from utils.token import exp
 
 
 class AuthPayload(BaseModel):
     user: str
     # registered claim #
     iss: str = iss
+    iat: int
+    exp: int
 
 
 def create_token(user: str) -> str:
     return encode(
         payload=AuthPayload(
             user=user,
+            iat=iat(),
+            exp=exp()
         ).dict(),
         key=key,
         algorithm=algorithms[0]
