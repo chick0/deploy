721a6c6b1ee2d25d03831d69f43478edb3be13bd
chick_0 <chick_0@ch1ck.xyz>
2022-07-17 19:30:22 +0900

    create login

--- v1/auth.py
+++ v1/auth.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
+from hashlib import sha512
+from datetime import datetime
 
+from fastapi import APIRouter
+from fastapi import HTTPException
+
+from sql import get_session
+from sql.models import User
 from v1.models.auth import LoginRequest
 from v1.models.auth import LoginResponse
 from utils.token.auth import create_token
@@ -15,4 +21,30 @@ router = APIRouter(
     response_model=LoginResponse
 )
 async def create_token_for_login(request: LoginRequest):
-    return {}
+    session = get_session()
+    user: User = session.query(User).filter_by(
+        email=request.email,
+        password=sha512(request.password.encode()).hexdigest()
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "msg": "등록된 사용자가 아닙니다."
+            }
+        )
+
+    uuid = user.uuid
+
+    user.last_login_at = datetime.now()
+
+    try:
+        return LoginResponse(
+            token=create_token(
+                user=uuid
+            )
+        )
+    finally:
+        session.commit()
+        session.close()
