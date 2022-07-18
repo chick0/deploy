685bd6da5e8e30d934a6542c4285db76342ca9cb
chick_0 <chick_0@ch1ck.xyz>
2022-07-18 19:56:07 +0900

    add todo

--- v1/pull.py
+++ v1/pull.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from fastapi import Depends
 
+from sql import get_session
+from sql.models import Project
 from v1.const import deploy
 from v1.models.pull import PullResult
+from utils.token.deploy import parse_token
+from utils.type import ProjectType
 
 router = APIRouter(
     tags=["Pull"]
@@ -10,10 +14,39 @@ router = APIRouter(
 
 
 @router.post(
-    "/pull/:uuid",
+    "/pull",
     description="소스코드의 변경 사항을 적용합니다.",
     response_model=PullResult
 )
 # pylint: disable=missing-function-docstring
-async def pull_and_deploy(uuid: str, token=Depends(deploy)):
+async def pull_and_deploy(token=Depends(deploy)):
+    payload = parse_token(token=token)
+    if not payload.permission.write:
+        return PullResult(
+            result=False,
+            reason="쓰기 권한이 없어 해당 토큰을 사용 할 수 없습니다."
+        )
+
+    session = get_session()
+    project: Project = session.query(Project).filter_by(
+        uuid=payload.project
+    ).first()
+
+    if project is None:
+        session.close()
+        return PullResult(
+            result=False,
+            reason="등록된 프로젝트가 아닙니다."
+        )
+
+    if project.type == ProjectType.FRONTEND.value:
+        session.close()
+        return PullResult(
+            result=False,
+            reason="프론트 프로젝트는 해당 방식으로 배포 할 수 없습니다."
+        )
+
+    # TODO:git pull
+    # TODO:restart with command
+
     return {}
--- v1/upload.py
+++ v1/upload.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import UploadFile
 
+from sql import get_session
+from sql.models import Project
 from v1.const import deploy
 from v1.models.upload import UploadResult
+from utils.token.deploy import parse_token
+from utils.type import ProjectType
 
 router = APIRouter(
     tags=["Upload"]
@@ -11,10 +15,50 @@ router = APIRouter(
 
 
 @router.post(
-    "/upload/:uuid",
+    "/upload",
     description="배포할 파일을 압축 파일로 업로드 합니다.",
     response_model=UploadResult
 )
 # pylint: disable=missing-function-docstring
-async def upload_and_deploy(uuid: str, file: UploadFile, token=Depends(deploy)):
+async def upload_and_deploy(file: UploadFile, token=Depends(deploy)):
+    payload = parse_token(token=token)
+    if not payload.permission.write:
+        return UploadResult(
+            result=False,
+            reason="쓰기 권한이 없어 해당 토큰을 사용 할 수 없습니다."
+        )
+
+    session = get_session()
+
+    project: Project = session.query(Project).filter_by(
+        uuid=payload.project
+    ).first()
+
+    if project is None:
+        session.close()
+        return UploadResult(
+            result=False,
+            reason="등록된 프로젝트가 아닙니다."
+        )
+
+    if project.type != ProjectType.FRONTEND.value:
+        session.close()
+        return UploadResult(
+            result=False,
+            reason="프론트 프로젝트만 해당 방식으로 배포 할 수 있습니다."
+        )
+
+    stream = await file.read()
+
+    head = stream[:3]
+    if not head.startswith(b"PK"):
+        session.close()
+        return UploadResult(
+            result=False,
+            reason="압축 파일을 업로드 해야 합니다.."
+        )
+
+    # TODO:remove old file
+    # TODO:unzip and move to project path
+
     return {}
