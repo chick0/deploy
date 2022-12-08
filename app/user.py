3607f99eb84ec2c826646907090ab83a7dd46d92
chick_0 <chick_0@ch1ck.xyz>
2022-12-09 06:19:17 +0900

    fix: Login session crashed
    When password is changed or user deleted, redirect to logout page not login page

--- app/user.py
+++ app/user.py
@@ -23,17 +23,17 @@ def login_required(f):
             user_password = session['user.password']
         except KeyError:
             flash("로그인이 필요합니다.")
-            raise RedirectRequired(url_for("auth.login"))
+            raise RedirectRequired(url_for("auth.logout"))
 
         user = get_user(user_id)
 
         if user is None:
             flash("삭제된 계정입니다.")
-            raise RedirectRequired(url_for("auth.login"))
+            raise RedirectRequired(url_for("auth.logout"))
 
         if user.password != user_password:
             flash("비밀번호가 변경되었습니다. 다시 로그인해주세요.")
-            raise RedirectRequired(url_for("auth.login"))
+            raise RedirectRequired(url_for("auth.logout"))
 
         return f(*args, **kwargs, user=user)
 
