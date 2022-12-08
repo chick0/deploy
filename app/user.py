d129c40b098e4f9276b69aed3e2c8d803551be64
chick_0 <chick_0@ch1ck.xyz>
2022-12-09 05:17:10 +0900

    fix: Login check decorator circulation redirect problem

--- app/user.py
+++ app/user.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from typing import Optional
 
 from flask import session
 from flask import flash
@@ -8,30 +9,32 @@ from .error import RedirectRequired
 from .models import User
 
 
-def get_user():
-    try:
-        user_id = session['user.id']
-        user_password = session['user.password']
-    except KeyError:
-        flash("로그인이 필요합니다.")
-        raise RedirectRequired(url_for("auth.login"))
-
-    user = User.query.filter_by(
+def get_user(user_id: int) -> Optional[User]:
+    return User.query.filter_by(
         id=user_id,
-        password=user_password
     ).first()
-
-    if user is None:
-        flash("삭제된 계정이거나 비밀번호가 변경되었습니다. 다시 로그인해주세요.")
-        raise RedirectRequired(url_for("auth.login"))
-
-    return user
 
 
 def login_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        user = get_user()
+        try:
+            user_id = session['user.id']
+            user_password = session['user.password']
+        except KeyError:
+            flash("로그인이 필요합니다.")
+            raise RedirectRequired(url_for("auth.login"))
+
+        user = get_user(user_id)
+
+        if user is None:
+            flash("삭제된 계정입니다.")
+            raise RedirectRequired(url_for("auth.login"))
+
+        if user.password != user_password:
+            flash("비밀번호가 변경되었습니다. 다시 로그인해주세요.")
+            raise RedirectRequired(url_for("auth.login"))
+
         return f(*args, **kwargs, user=user)
 
     return decorator
@@ -42,10 +45,13 @@ def login_not_required(f):
     def decorator(*args, **kwargs):
         try:
             user_id = session['user.id']
-
-            if user_id <= 0:
-                raise KeyError
+            user_password = session['user.password']
         except KeyError:
+            return f(*args, **kwargs)
+
+        user = get_user(user_id)
+
+        if user is None or user.password != user_password:
             return f(*args, **kwargs)
 
         raise RedirectRequired(url_for("project.get_list"))
