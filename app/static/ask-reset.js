1c03f6b34fb705c65a824cc94adbf2fa64036a55
chick_0 <chick_0@ch1ck.xyz>
2022-12-15 20:07:37 +0900

    chore: Remove inline javascript for use prettier

---
+++ app/static/ask-reset.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} url
+ */
+function ask_reset(url) {
+    if (confirm("해당 계정의 비밀번호를 삭제하고 임시 비밀번호로 설정하시겠습니까?")) {
+        location.href = url;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("button.ask-reset").forEach((element) => {
+        element.addEventListener("click", (event) => {
+            const href = event.currentTarget.dataset.href;
+            ask_reset(href);
+        });
+    });
+});
