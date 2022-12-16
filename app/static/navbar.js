c19bb8f14631e52baad7b46c593ddd2e86a1662d
chick_0 <chick_0@ch1ck.xyz>
2022-12-16 13:55:39 +0900

    chore: Remove not required variable

--- app/static/navbar.js
+++ app/static/navbar.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const el = document.querySelectorAll(".navbar-burger")[0];
+    const el = document.querySelector(".navbar-burger");
 
     el.addEventListener("click", () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+        const target = document.getElementById(el.dataset.target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
+        target.classList.toggle("is-active");
     });
 });
